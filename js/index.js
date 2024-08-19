cc9d11061affc5da18ffecc8520c6057ffd8d297
samminmul <74859160+samminmul@users.noreply.github.com>
2024-08-19 21:19:58 +0900

    사소한수정
    네자릿수까진 읽음

--- js/index.js
+++ js/index.js
@@ -5,29 +5,31 @@ audio.src = "../sound/Ding1s.mp3";
 //audio.duration = 0.6;
 
 function ReadOneNumber(number0to9, suffix){
-    const read = "영일이삼사오육칠팔구";
-    if (number0to9 === 0) {
-        return "";
-    }
-    if (suffix === "") {
-        return read[number0to9];
-    }
-    if (number0to9 === 1) {
-        return suffix;
-    }
-    return read[number0to9] + suffix;
+  const read = "영일이삼사오육칠팔구";
+  if (number0to9 === 0) {
+    return "";
+  }
+  if (suffix === "") {
+    return read[number0to9];
+  }
+  if (number0to9 === 1) {
+    return suffix;
+  }
+  return read[number0to9] + suffix;
 }
 
 function ReadNumber(number){ //숫자가 천의 자리를 넘는다면 지구멸망함
-    let hundred = parseInt(number / 100);
-    let ten = parseInt(number / 10) % 10;
-    let one = number % 10;
+  let thousand = parseInt(number / 1000);
+  let hundred = parseInt(number / 100) % 10;
+  let ten = parseInt(number / 10) % 100;
+  let one = number % 10;
 
-    let read = ReadOneNumber(hundred, "백") + 
-               ReadOneNumber(ten, "십") + 
-               ReadOneNumber(one, "")
-    
-    return read;
+  let read = ReadOneNumber(thousand, "천") + 
+            ReadOneNumber(hundred, "백") + 
+            ReadOneNumber(ten, "십") + 
+            ReadOneNumber(one, "")
+  
+  return read;
 }
 
 socketClient.on("호출", (number) => {
